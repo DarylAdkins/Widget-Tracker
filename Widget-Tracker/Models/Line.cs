830735b7738dacfa24cddc38b3f81b5aa4d07174
Daryl Adkins <daryl.adkins@gmail.com>
2020-01-24 11:53:07 -0500

    first branch, includes partial models

---
+++ Widget-Tracker/Models/Line.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Widget_Tracker.Models
+{
+    public class Line
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int Name { get; set; }
+
+        public List<Process> Processes { get; set; } = new List<Process>();
+
+    }
+}
---
+++ Widget-Tracker/Models/Lot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Widget_Tracker.Models
+{
+    public class Lot
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string LineId { get; set; }
+
+        public string UserId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [Display(Name = "Date Created")]
+        public DateTime DateCreated { get; set; }
+    }
+}
